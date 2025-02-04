f1ec5d7bb5af792579ec89c4cd2fd2636fd1cc1d
André Luiz Duarte Cruz <And969696@outlook.com>
2025-02-04 18:08:27 -0300

    ajustes no controller

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,13 +3,20 @@ import jwt from 'jsonwebtoken';
 export const isAuthenticated = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
 
+  // Log para verificar se o token foi extraído corretamente
+  console.log('Token extraído:', token);
+
   if (!token) {
     return res.status(401).json({ error: 'Acesso não autorizado' });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.id; // Adiciona o ID do usuário à requisição
+
+    // Log para verificar o conteúdo do token decodificado
+    console.log('Token decodificado:', decoded);
+
+    req.user = decoded; // Adiciona o usuário decodificado ao objeto de requisição
     next();
   } catch (error) {
     return res.status(401).json({ error: 'Token inválido ou expirado' });
